ca5be40a0c9491ac86d80ed22535e79217f2b4e3
taguhiabgar <taguhiabgar@gmail.com>
2017-07-12 16:35:53 +0400

    Watchdog Inspector added, and blending issues resolved

--- SL/DiscussionsTableViewCell.swift
+++ SL/DiscussionsTableViewCell.swift
@@ -9,103 +9,189 @@
 import UIKit
 
 class DiscussionsTableViewCell: UITableViewCell {
+  
+  // MARK: - Properties
+  
+  // left side
+  
+  @IBOutlet weak var leftView: UIView!
+  @IBOutlet weak var votesLabel: UILabel!
+  @IBOutlet weak var answersLabel: UILabel!
+  
+  // right side
+  
+  @IBOutlet weak var rightView: UIStackView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var authorLabel: UILabel!
+  @IBOutlet weak var bottomStackView: UIStackView!
+  @IBOutlet weak var tagsVerticalView: UIView!
+  
+  var tagsView: UIView!
+  
+  // MARK: - Methods
+  
+  func drawContent(content: DiscussionContent) {
+    votesLabel.text = "\(content.votesCount)"
+    answersLabel.text = "\(content.answersCount)"
+    dateLabel.text = formattedDate(content.date)
+    authorLabel.text = content.author
     
-    // MARK: - Properties
-    
-    // left side 
-    
-    @IBOutlet weak var leftView: UIView!
-    @IBOutlet weak var votesLabel: UILabel!
-    @IBOutlet weak var answersLabel: UILabel!
-    
-    // right side
-    
-    @IBOutlet weak var rightView: UIStackView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
-    @IBOutlet weak var authorLabel: UILabel!
-    @IBOutlet weak var bottomStackView: UIStackView!
-    @IBOutlet weak var tagsVerticalView: UIView!
-    
-    var tagsView: UIView!
-    
-    // MARK: - Methods
-    
-    func drawContent(content: DiscussionContent) {
-        votesLabel.text = "\(content.votesCount)"
-        answersLabel.text = "\(content.answersCount)"
-        titleLabel.text = content.title.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
-        dateLabel.text = formattedDate(content.date)
-        authorLabel.text = content.author
-        
-        // configure title label
-        titleLabel.lineBreakMode = .byWordWrapping
-        
-        // tags view
-        setupTagsView(with: content.tags)
-        
-        tagsVerticalView.addSubview(tagsView)
-        // add contraints from top and bottom so that tagsVerticalView will have correct height
-        let tagsViewConstraints: [NSLayoutConstraint] = [
-            NSLayoutConstraint(item: self.tagsView, attribute: .top, relatedBy: .equal, toItem: tagsVerticalView, attribute: .top, multiplier: 1,
-                               constant: 0),
-            NSLayoutConstraint(item: self.tagsView, attribute: .bottom, relatedBy: .equal, toItem: tagsVerticalView, attribute: .bottom, multiplier: 1, constant: 0)
-        ]
-        self.addConstraints(tagsViewConstraints)
+//    DispatchQueue.main.async {
+      // configure title label
+      self.titleLabel.text = content.title.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+      self.titleLabel.lineBreakMode = .byWordWrapping
+      
+      // tags view
+//      self.setupTagsView(with: content.tags)
+//      
+//      self.tagsVerticalView.addSubview(self.tagsView)
+//      // add contraints from top and bottom so that tagsVerticalView will have correct height
+//      let tagsViewConstraints: [NSLayoutConstraint] = [
+//        NSLayoutConstraint(item: self.tagsView, attribute: .top, relatedBy: .equal, toItem: self.tagsVerticalView, attribute: .top, multiplier: 1,
+//                           constant: 0),
+//        NSLayoutConstraint(item: self.tagsView, attribute: .bottom, relatedBy: .equal, toItem: self.tagsVerticalView, attribute: .bottom, multiplier: 1, constant: 0)
+//      ]
+//      self.addConstraints(tagsViewConstraints)
+//    }
+  }
+  
+  func formattedDate(_ date: Date) -> String {
+    let dateformatter = DateFormatter()
+    dateformatter.dateStyle = DateFormatter.Style.short
+    dateformatter.timeStyle = DateFormatter.Style.short
+    dateformatter.dateFormat = "MM/dd/yy h:mm a"
+    let dateString = dateformatter.string(from: date)
+    return dateString
+  }
+  
+  private func setupTagsView(with tags: [String]) {
+    // remove subviews
+    if tagsView != nil {
+      for subview in tagsView.subviews {
+        subview.removeFromSuperview()
+      }
+    } else {
+      tagsView = UIView()
     }
     
-    func formattedDate(_ date: Date) -> String {
-        let dateformatter = DateFormatter()
-        dateformatter.dateStyle = DateFormatter.Style.short
-        dateformatter.timeStyle = DateFormatter.Style.short
-        dateformatter.dateFormat = "MM/dd/yy h:mm a"
-        let dateString = dateformatter.string(from: date)
-        return dateString
-    }
+    var lastX: CGFloat = 0
+    var lastY: CGFloat = 0
+    var tagsMaxY: CGFloat = 0
     
-    private func setupTagsView(with tags: [String]) {
-        // remove subviews
-        if tagsView != nil {
-            for subview in tagsView.subviews {
-                subview.removeFromSuperview()
-            }
-        } else {
-            tagsView = UIView()
-        }
-        
-        var lastX: CGFloat = 0
-        var lastY: CGFloat = 0        
-        var tagsMaxY: CGFloat = 0
-        
-        // create labels
-        for tag in tags {
-            let tagLabel = UILabel()
-            tagLabel.text = tag
-            tagLabel.font = dateLabel.font
-            tagLabel.sizeToFit()
-            tagLabel.textColor = UIColor.white
-            tagLabel.textAlignment = .center
-            tagLabel.frame.size.width = tagLabel.intrinsicContentSize.width + 2 * Constants.discussionsTCTagTitleHorizontalMargin
-            
-            // for smooth scrolling
-            tagLabel.backgroundColor = UIColor.clear
-            tagLabel.layer.backgroundColor = Constants.discussionsTCTagColor.cgColor
-            tagLabel.layer.cornerRadius = 4
-            tagLabel.layer.masksToBounds = false
-            tagLabel.layer.shouldRasterize = true
-            tagLabel.layer.rasterizationScale = UIScreen.main.scale
-            
-            // calculate position of label
-            if lastX + tagLabel.frame.width >= titleLabel.frame.width {
-                lastX = 0
-                lastY += tagLabel.frame.height + Constants.discussionsTCTagsVerticalPadding
-            }
-            tagLabel.frame.origin = CGPoint(x: lastX, y: lastY)
-            lastX += tagLabel.frame.width + Constants.discussionsTCTagsHorizontalPadding
-            tagsMaxY = tagLabel.frame.height + lastY
-            
-            tagsView.addSubview(tagLabel)
-        }
-        tagsView.frame.size.height = tagsMaxY
+    // create labels
+    for tag in tags {
+      let tagLabel = UILabel()
+      
+      tagLabel.layer.drawsAsynchronously = true
+      
+      tagLabel.layer.isOpaque = true
+      tagLabel.text = tag
+      tagLabel.font = dateLabel.font
+      tagLabel.sizeToFit()
+      tagLabel.textColor = UIColor.white
+      tagLabel.textAlignment = .center
+      tagLabel.frame.size.width = tagLabel.intrinsicContentSize.width + 2 * Constants.discussionsTCTagTitleHorizontalMargin
+      
+      // for smooth scrolling
+      tagLabel.backgroundColor = Constants.discussionsTCTagColor
+      tagLabel.layer.backgroundColor = UIColor.white.cgColor
+      tagLabel.layer.cornerRadius = 4
+      tagLabel.layer.masksToBounds = false
+      tagLabel.layer.shouldRasterize = true
+      tagLabel.layer.rasterizationScale = UIScreen.main.scale
+      
+      // calculate position of label
+      if lastX + tagLabel.frame.width >= titleLabel.frame.width {
+        lastX = 0
+        lastY += tagLabel.frame.height + Constants.discussionsTCTagsVerticalPadding
+      }
+      tagLabel.frame.origin = CGPoint(x: lastX, y: lastY)
+      lastX += tagLabel.frame.width + Constants.discussionsTCTagsHorizontalPadding
+      tagsMaxY = tagLabel.frame.height + lastY
+      
+      tagsView.addSubview(tagLabel)
     }
+    tagsView.frame.size.height = tagsMaxY
+  }
+  
+//  func f() {
+//    // setup text handling
+//    var textStorage = NSTextStorage(attributedString: NSAttributedString(string: "Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu"))
+//    
+//    textStorage.addLayoutManager(NSLayoutManager())
+//    var textContainer = NSTextContainer(size: <#T##CGSize#>)
+//    
+//  }
+  
 }
+  
+//  // use our subclass of NSLayoutManager
+//  MyLayoutManager *textLayout = [[MyLayoutManager alloc] init];
+//  
+//  [textStorage addLayoutManager:textLayout];
+//  
+//  NSTextContainer *textContainer = [[NSTextContainer alloc] initWithSize:self.view.bounds.size];
+//  
+//  [textLayout addTextContainer:textContainer];
+//  UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake(0,20,self.view.bounds.size.width,self.view.bounds.size.height-20)
+//  textContainer:textContainer];
+//  [self.view addSubview:textView];
+//  
+//  // set some background color to our text
+//  [textView.textStorage setAttributes:[NSDictionary dictionaryWithObject:[UIColor blueColor] forKey:NSBackgroundColorAttributeName] range:NSMakeRange(22, textView.text.length - 61)];
+//}
+//@end
+//
+//@interface MyLayoutManager : NSLayoutManager
+//@end
+//
+//- (void)fillBackgroundRectArray:(const CGRect *)rectArray count:(NSUInteger)rectCount forCharacterRange:(NSRange)charRange color:(UIColor *)color
+//{
+//  CGFloat halfLineWidth = 4.; // change this to change corners radius
+//  
+//  CGMutablePathRef path = CGPathCreateMutable();
+//  
+//  if (rectCount == 1
+//    || (rectCount == 2 && (CGRectGetMaxX(rectArray[1]) < CGRectGetMinX(rectArray[0])))
+//    )
+//  {
+//    // 1 rect or 2 rects without edges in contact
+//    
+//    CGPathAddRect(path, NULL, CGRectInset(rectArray[0], halfLineWidth, halfLineWidth));
+//    if (rectCount == 2)
+//    CGPathAddRect(path, NULL, CGRectInset(rectArray[1], halfLineWidth, halfLineWidth));
+//  }
+//  else
+//  {
+//    // 2 or 3 rects
+//    NSUInteger lastRect = rectCount - 1;
+//    
+//    CGPathMoveToPoint(path, NULL, CGRectGetMinX(rectArray[0]) + halfLineWidth, CGRectGetMaxY(rectArray[0]) + halfLineWidth);
+//    
+//    CGPathAddLineToPoint(path, NULL, CGRectGetMinX(rectArray[0]) + halfLineWidth, CGRectGetMinY(rectArray[0]) + halfLineWidth);
+//    CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(rectArray[0]) - halfLineWidth, CGRectGetMinY(rectArray[0]) + halfLineWidth);
+//    
+//    CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(rectArray[0]) - halfLineWidth, CGRectGetMinY(rectArray[lastRect]) - halfLineWidth);
+//    CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(rectArray[lastRect]) - halfLineWidth, CGRectGetMinY(rectArray[lastRect]) - halfLineWidth);
+//    
+//    CGPathAddLineToPoint(path, NULL, CGRectGetMaxX(rectArray[lastRect]) - halfLineWidth, CGRectGetMaxY(rectArray[lastRect]) - halfLineWidth);
+//    CGPathAddLineToPoint(path, NULL, CGRectGetMinX(rectArray[lastRect]) + halfLineWidth, CGRectGetMaxY(rectArray[lastRect]) - halfLineWidth);
+//    
+//    CGPathAddLineToPoint(path, NULL, CGRectGetMinX(rectArray[lastRect]) + halfLineWidth, CGRectGetMaxY(rectArray[0]) + halfLineWidth);
+//    
+//    CGPathCloseSubpath(path);
+//  }
+//  
+//  [color set]; // set fill and stroke color
+//  
+//  CGContextRef ctx = UIGraphicsGetCurrentContext();
+//  CGContextSetLineWidth(ctx, halfLineWidth * 2.);
+//  CGContextSetLineJoin(ctx, kCGLineJoinRound);
+//  
+//  CGContextAddPath(ctx, path);
+//  CGPathRelease(path);
+//  
+//  CGContextDrawPath(ctx, kCGPathFillStroke);
+//}
+//
--- SL/ViewController.swift
+++ SL/ViewController.swift
@@ -9,139 +9,151 @@
 import UIKit
 
 class ViewController: UIViewController {
+  
+  // MARK: - Properties
+  
+  var tableView: UITableView!
+  
+  // contains models of all loaded discussions
+  var discussions = [DiscussionContent]()
+  
+  // shows index of file which will be loaded when requested
+  var nextResourceIndex = 0
+  
+  // MARK: - Lifecycle
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
     
-    // MARK: - Properties
+    // tableView basic setup
+    tableView = UITableView(frame: view.frame)
+    view.addSubview(tableView)
+    tableView.delegate = self
+    tableView.dataSource = self
     
-    var tableView: UITableView!
+    // register nib file
+    tableView.register(UINib.init(nibName: Constants.discussionsTCNibName, bundle: nil), forCellReuseIdentifier: Constants.discussionsTCIdentifier)
     
-    // contains models of all loaded discussions
-    var discussions = [DiscussionContent]() {
-        didSet {
-            DispatchQueue.main.async {
-                self.updateView()
-            }
-        }
+    // configure row height
+    tableView.rowHeight = UITableViewAutomaticDimension
+    tableView.estimatedRowHeight = 70
+    
+    //        tableView.allowsSelection = false
+    
+    // read data from file
+    perform(#selector(requestData), with: nil, afterDelay: 2.0)
+  }
+  
+  // MARK: - Methods
+  
+  private func convertToDiscussions(array: Array<Dictionary<String, Any>>) -> Array<DiscussionContent> {
+    var result: Array<DiscussionContent> = []
+    for dictionary in array {
+      let title = dictionary[Constants.discussionTitleKeyInJSON] as? String ?? Constants.discussionsDefaultTitleValue
+      let tags = dictionary[Constants.discussionTagsKeyInJSON] as? [String] ?? Constants.discussionsDefaultTagsValue
+      let votesCount = dictionary[Constants.discussionVotesCountKeyInJSON] as? Int ?? Constants.discussionsDefaultVotesValue
+      let answersCount = dictionary[Constants.discussionAnswersCountKeyInJSON] as? Int ?? Constants.discussionsDefaultAnswersValue
+      let date = dictionary[Constants.discussionDateKeyInJSON] as? Date ?? Constants.discussionsDefaultDateValue
+      let author = dictionary[Constants.discussionAuthorKeyInJSON] as? String ?? Constants.discussionsDefaultAuthorValue
+      let content = DiscussionContent(title: title, tags: tags, votesCount: votesCount, answersCount: answersCount, date: date, author: author)
+      result.append(content)
     }
-    
-    // shows index of file which will be loaded when requested
-    var nextResourceIndex = 0
-    
-    // MARK: - Lifecycle
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // tableView basic setup
-        tableView = UITableView(frame: view.frame)
-        view.addSubview(tableView)
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        // register nib file
-        tableView.register(UINib.init(nibName: Constants.discussionsTCNibName, bundle: nil), forCellReuseIdentifier: Constants.discussionsTCIdentifier)
-        
-        // configure row height
-        tableView.rowHeight = UITableViewAutomaticDimension
-        tableView.estimatedRowHeight = 50
-        
-        tableView.allowsSelection = false
-        
-        // read data from file
-        perform(#selector(requestData), with: nil, afterDelay: 2.0)
-    }
-    
-    // MARK: - Methods
-    
-    private func convertToDiscussions(array: Array<Dictionary<String, Any>>) -> Array<DiscussionContent> {
-        var result: Array<DiscussionContent> = []
-        for dictionary in array {
-            let title = dictionary[Constants.discussionTitleKeyInJSON] as? String ?? Constants.discussionsDefaultTitleValue
-            let tags = dictionary[Constants.discussionTagsKeyInJSON] as? [String] ?? Constants.discussionsDefaultTagsValue
-            let votesCount = dictionary[Constants.discussionVotesCountKeyInJSON] as? Int ?? Constants.discussionsDefaultVotesValue
-            let answersCount = dictionary[Constants.discussionAnswersCountKeyInJSON] as? Int ?? Constants.discussionsDefaultAnswersValue
-            let date = dictionary[Constants.discussionDateKeyInJSON] as? Date ?? Constants.discussionsDefaultDateValue
-            let author = dictionary[Constants.discussionAuthorKeyInJSON] as? String ?? Constants.discussionsDefaultAuthorValue
-            let content = DiscussionContent(title: title, tags: tags, votesCount: votesCount, answersCount: answersCount, date: date, author: author)
-            result.append(content)
-        }
-        return result
-    }
-    
-    @objc fileprivate func requestData() {
-        // create a concurrent queue and perform data requesting inside it
-        let queue = DispatchQueue.init(label: "", qos: DispatchQoS.background, attributes: DispatchQueue.Attributes.concurrent, autoreleaseFrequency: DispatchQueue.AutoreleaseFrequency.never, target: nil)
-        queue.async {
-            let path = self.nextResourceName()
-            do {
-                let text = try String(contentsOfFile: path)
-                if let objectData = text.data(using: String.Encoding.utf8) {
-                    do {
-                        let jsonObject = try JSONSerialization.jsonObject(with: objectData, options: JSONSerialization.ReadingOptions.mutableContainers)
-                        if let outerDictionary = jsonObject as? Dictionary<String, Any> {
-                            if let array = outerDictionary["posts"] as? Array<Dictionary<String, Any>> {
-                                let result = self.convertToDiscussions(array: array)
-                                self.discussions.append(contentsOf: result)
-                                DispatchQueue.main.async {
-                                    self.updateView()
-                                }
-                            }
-                        }
-                    } catch {
-                        print(error)
-                    }
+    return result
+  }
+  
+  @objc fileprivate func requestData() {
+    // create a concurrent queue and perform data requesting inside it
+    let queue = DispatchQueue.init(label: "", qos: DispatchQoS.background, attributes: DispatchQueue.Attributes.concurrent, autoreleaseFrequency: DispatchQueue.AutoreleaseFrequency.never, target: nil)
+    queue.async {
+      let path = self.nextResourceName()
+      do {
+        let text = try String(contentsOfFile: path)
+        if let objectData = text.data(using: String.Encoding.utf8) {
+          do {
+            let jsonObject = try JSONSerialization.jsonObject(with: objectData, options: JSONSerialization.ReadingOptions.mutableContainers)
+            if let outerDictionary = jsonObject as? Dictionary<String, Any> {
+              if let array = outerDictionary["posts"] as? Array<Dictionary<String, Any>> {
+                let result = self.convertToDiscussions(array: array)
+                self.discussions.append(contentsOf: result)
+//                // get indexPaths to update
+//                var rowsToUpdate: [IndexPath] = []
+//                for i in 0..<result.count {
+//                  rowsToUpdate.append(IndexPath(row: self.discussions.count - i - 1, section: 0))
+//                }
+                DispatchQueue.main.async {
+                  self.updateView()
+                  // only update added rows
+//                  self.tableView.reloadRows(at: rowsToUpdate, with: UITableViewRowAnimation.automatic)
                 }
-            } catch {
-                print(error)
+              }
             }
+          } catch {
+            print(error)
+          }
         }
+      } catch {
+        print(error)
+      }
     }
-    
-    private func updateView() {
-        tableView.reloadData()
+  }
+  
+  private func updateView() {
+    tableView.reloadData()
+  }
+  
+  private func nextResourceName() -> String {
+    let next = Constants.allResources[nextResourceIndex]
+    nextResourceIndex += 1
+    if nextResourceIndex == Constants.allResources.count {
+      nextResourceIndex = 0
     }
-    
-    private func nextResourceName() -> String {
-        let next = Constants.allResources[nextResourceIndex]
-        nextResourceIndex += 1
-        if nextResourceIndex == Constants.allResources.count {
-            nextResourceIndex = 0
-        }
-        return next!
-    }
-    
+    return next!
+  }
+  
 }
 
 // MARK: - TableView Delegate and Datasource
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return discussions.isEmpty ? 20 : discussions.count
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return discussions.isEmpty ? 20 : discussions.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: Constants.discussionsTCIdentifier)
+    return cell!
+  }
+  
+  func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    // draw content on cell
+    if indexPath.row < discussions.count {
+      let discussionCell = cell as? DiscussionsTableViewCell
+      discussionCell?.drawContent(content: discussions[indexPath.row])
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.discussionsTCIdentifier)
-        return cell!
+    // activity indicator
+    let lastSectionIndex = tableView.numberOfSections - 1
+    let lastRowIndex = tableView.numberOfRows(inSection: lastSectionIndex) - 1
+    if indexPath.section ==  lastSectionIndex && indexPath.row == lastRowIndex { // last row is reached
+      // create and show activity indicator
+      let spinner = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+      spinner.startAnimating()
+      spinner.frame = CGRect(x: CGFloat(0), y: CGFloat(0), width: tableView.bounds.width, height: CGFloat(44))
+      tableView.tableFooterView = spinner
+      tableView.tableFooterView?.isHidden = false
+      // load next page after 2 seconds
+      perform(#selector(requestData), with: nil, afterDelay: 2.0)
+    }
+  }
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if let cell = tableView.cellForRow(at: indexPath) as? DiscussionsTableViewCell {
+      print("x: \(cell.dateLabel.frame.origin.x) : \(cell.authorLabel.frame.origin.x)")
+      print("y: \(cell.dateLabel.frame.origin.y) : \(cell.authorLabel.frame.origin.y)")
     }
     
-    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        // draw content on cell
-        if indexPath.row < discussions.count {
-            let discussionCell = cell as? DiscussionsTableViewCell
-            discussionCell?.drawContent(content: discussions[indexPath.row])
-        }
-        
-        // activity indicator
-        let lastSectionIndex = tableView.numberOfSections - 1
-        let lastRowIndex = tableView.numberOfRows(inSection: lastSectionIndex) - 1
-        if indexPath.section ==  lastSectionIndex && indexPath.row == lastRowIndex { // last row is reached
-            // create and show activity indicator
-            let spinner = UIActivityIndicatorView(activityIndicatorStyle: .gray)
-            spinner.startAnimating()
-            spinner.frame = CGRect(x: CGFloat(0), y: CGFloat(0), width: tableView.bounds.width, height: CGFloat(44))
-            tableView.tableFooterView = spinner
-            tableView.tableFooterView?.isHidden = false
-            // load next page after 2 seconds
-            perform(#selector(requestData), with: nil, afterDelay: 2.0)
-        }
-    }
+    
+    print(indexPath.row)
+  }
+  
 }
